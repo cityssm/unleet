e22e5a42b9380866b2230f6406b980aa99f3c347
dangowans <d.gowans@cityssm.on.ca>
2021-07-16 14:46:31 -0400

    fixing a path finding issue

--- translations.js
+++ translations.js
@@ -1,5 +1,7 @@
 import * as fs from "fs";
-const translations = JSON.parse(fs.readFileSync("translations.json").toString());
+import { fileURLToPath } from "url";
+const translationsPath = fileURLToPath(new URL("translations.json", import.meta.url));
+const translations = JSON.parse(fs.readFileSync(translationsPath).toString());
 export const simpleTranslations = translations.simpleTranslations;
 export const complexTranslations = translations.complexTranslations;
 export const translationSymbols = [...Object.keys(simpleTranslations), ...Object.keys(complexTranslations)];
--- translations.ts
+++ translations.ts
@@ -1,8 +1,11 @@
 import * as fs from "fs";
+import { fileURLToPath } from "url";
 
 type Translations = { [symbol: string]: string[] };
 
-const translations = JSON.parse(fs.readFileSync("translations.json").toString());
+const translationsPath = fileURLToPath(new URL("translations.json", import.meta.url));
+
+const translations = JSON.parse(fs.readFileSync(translationsPath).toString());
 
 export const simpleTranslations: Translations = translations.simpleTranslations;
 export const complexTranslations: Translations = translations.complexTranslations;
